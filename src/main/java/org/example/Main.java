3de8b4d48ebe113ef7ec525a7bccc8a98dee3bd9
Bruno2204 <brunoegama@gmail.com>
2023-04-27 09:32:11 -0300

    puntaje mejorado

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -2,25 +2,41 @@ package org.example;
 
 import java.io.BufferedReader;
 import java.io.FileReader;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.Statement;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
+import java.util.Scanner;
 
 public class Main {
     public static HashMap<String,Partido> resultados = new HashMap<String,Partido>();
     public static HashMap<String,Integer> fases = new HashMap<String,Integer>();
     public static HashMap<String,Integer> rondas = new HashMap<String,Integer>();
+    public static int puntosXAcierto = 1;
 
     public static void main(String[] args) {
 
-        try (BufferedReader brResultados = new BufferedReader(new FileReader("src/main/resources/resultados.csv")) ;
-             BufferedReader brPronosticos = new BufferedReader(new FileReader("src/main/resources/pronosticos.csv"))) {
+        try {
 
-            List<String> fileResultados = brResultados.lines().toList();
-            AddPartidos(resultados,fileResultados);
+            //List<String> fileResultados = brResultados.lines().toList();
+            //List<String> filePronosticos = brPronosticos.lines().toList();
 
-            List<String> filePronosticos = brPronosticos.lines().toList();
-            AddPronosticos(filePronosticos);
+            Connection con = DriverManager.getConnection("jdbc:mysql://localhost:3306/aquiles","root","password");
+            Statement stmt = con.createStatement();
+
+            ResultSet rs = stmt.executeQuery("select * from resultados");
+            AddPartidos(resultados,rs);
+
+            rs = stmt.executeQuery("select * from pronosticos");
+            AddPronosticos(rs);
+
+            con.close();
+            Scanner scanner = new Scanner(System.in);
+            System.out.println("¿Cuántos puntos desea por acierto?");
+            puntosXAcierto = scanner.nextInt();
 
         } catch (Exception e) {
             e.printStackTrace();
@@ -29,8 +45,9 @@ public class Main {
         }
     }
 
-    public static void AddPartidos(HashMap<String,Partido> lista,List<String> file) {
+/*    public static void AddPartidos(HashMap<String,Partido> lista,List<String> file) {
         for (int i = 1 ; i < file.size() ; i++) {
+
             String[] datos = file.get(i).split(",");
             int fase = Integer.parseInt(datos[0]);
             int ronda = Integer.parseInt(datos[1]);
@@ -40,7 +57,9 @@ public class Main {
             Equipo equipo2 = Equipo.GetEquipo(datos[5]);
             Partido partido = new Partido(fase,ronda,equipo1,goles1,goles2,equipo2);
             lista.put(datos[2]+datos[5],partido);
+
             if (!rondas.containsKey(datos[1])){
+
                 rondas.put(datos[1],1);
                 if (!fases.containsKey(datos[0])){
                     fases.put(datos[0],1);
@@ -51,9 +70,37 @@ public class Main {
                 rondas.put(datos[1],rondas.get(datos[1])+1);
             }
         }
+    }*/
+    public static void AddPartidos(HashMap<String,Partido> lista,ResultSet file) {
+        try{
+            while (file.next()) {
+                int fase = file.getInt(1);
+                int ronda = file.getInt(2);
+                Equipo equipo1 = Equipo.GetEquipo(file.getString(3));
+                int goles1 = file.getInt(4);
+                int goles2 = file.getInt(5);
+                Equipo equipo2 = Equipo.GetEquipo(file.getString(6));
+                Partido partido = new Partido(fase,ronda,equipo1,goles1,goles2,equipo2);
+                lista.put(file.getString(3)+file.getString(6),partido);
+
+                if (!rondas.containsKey(file.getString(2))){
+
+                    rondas.put(file.getString(2),1);
+                    if (!fases.containsKey(file.getString(1))){
+                        fases.put(file.getString(1),1);
+                        }else {
+                        fases.put(file.getString(1),fases.get(file.getString(1))+1);
+                        }
+                }else {
+                    rondas.put(file.getString(2),rondas.get(file.getString(2))+1);
+                    }
+            }
+        } catch (Exception e){
+            e.printStackTrace();
+        }
     }
 
-    public static void AddPronosticos(List<String> file) {
+/*public static void AddPronosticos(List<String> file) {
         for (int i = 1 ; i < file.size() ; i++) {
             String[] datos = file.get(i).split(",");
             int ganador = 0;
@@ -66,7 +113,26 @@ public class Main {
             Participante.GetParticipante(datos[0]).pronosticos
                     .add(new Pronostico(ganador,partido));
         }
+    }*/
+
+    public static void AddPronosticos(ResultSet file) {
+        try {
+            while (file.next()) {
+                int ganador = 0;
+                if (file.getString(3).contains("x")){
+                    ganador = 1;
+                } else if (file.getString(5).contains("x")) {
+                    ganador = 2;
+                }
+                Partido partido = resultados.get(file.getString(2)+file.getString(6));
+                Participante.GetParticipante(file.getString(1)).pronosticos
+                        .add(new Pronostico(ganador,partido));
+            }
+        } catch (Exception e){
+            e.printStackTrace();
+        }
     }
+
     public static void MostrarPuntajes() {
         Participante.participantes.forEach((key,participante)->{
             participante.ObtenerPuntaje();
--- src/main/java/org/example/Participante.java
+++ src/main/java/org/example/Participante.java
@@ -32,8 +32,8 @@ public class Participante {
     public void setNombre(String nombre) {
         this.nombre = nombre;
     }
-
-    public void ObtenerPuntaje() {
+/*
+    public void ObtenerPuntajee() {
         puntaje = 0;
         aciertos = 0;
         int faseEntera = 0;
@@ -50,9 +50,11 @@ public class Participante {
             if (pronostico.acierto) {
                 aciertosRonda++;
                 aciertos++;
+                System.out.println("AciertosRonda:"+aciertosRonda+" rondaLen:"+ rondaLen+" rondaLoopeando"+rondaActual+" rondaActual:"+pronostico.partido.ronda);
                 if (aciertosRonda == rondaLen && rondaActual == pronostico.partido.ronda){
                     rondaEntera++;
                     aciertosFase++;
+                    System.out.println("AciertosFase:"+aciertosFase+" FaseLen:"+ faseLen+" FaseLoopeando"+faseActual+" FaseActual:"+pronostico.partido.fase);
                     if (aciertosFase == faseLen && faseActual == pronostico.partido.fase){
                         faseEntera++;
                         aciertosFase=0;
@@ -62,6 +64,7 @@ public class Participante {
                         faseLen = Main.fases.get(Integer.toString(pronostico.partido.fase));
                     }
                 } else if (!(rondaActual == pronostico.partido.ronda)){
+                    System.out.println("AciertosFase:"+aciertosFase+" FaseLen:"+ faseLen+" FaseLoopeando"+faseActual+" FaseActual:"+pronostico.partido.fase);
                     aciertosRonda = 1;
                     rondaActual = pronostico.partido.ronda;
                     rondaLen = Main.rondas.get(Integer.toString(pronostico.partido.ronda));
@@ -69,6 +72,44 @@ public class Participante {
             }
         }
         puntaje = aciertos + rondaEntera*10 + faseEntera*100;
+    }*/
+
+    public void ObtenerPuntaje() {
+        puntaje = 0;
+        aciertos = 0;
+        int faseEntera = 0;
+        int rondaActual = 1;
+        int aciertosRonda = 0;
+        int rondaLen;
+        int faseLen;
+        int aciertosFase = 0;
+        int faseActual = 1;
+        for (int i = 0; i < this.pronosticos.size(); i++) {
+            Pronostico pronostico = pronosticos.get(i);
+            rondaLen = Main.rondas.get(Integer.toString(pronostico.partido.ronda));
+            faseLen = Main.fases.get(Integer.toString(pronostico.partido.fase));
+            if (pronostico.acierto) {
+                aciertos++;
+                aciertosRonda++;
+                if (!(pronostico.partido.ronda == rondaActual)){
+                    rondaActual = pronostico.partido.ronda;
+                    aciertosRonda=1;
+                }
+                if (aciertosRonda == rondaLen){
+                    aciertosFase++;
+                    rondaEntera++;
+                    if (!(pronostico.partido.fase == faseActual)){
+                        faseActual = pronostico.partido.fase;
+                        aciertosFase=1;
+                    }
+                    //System.out.println("Aciertos fase:"+aciertosFase+" Fase len:"+ faseLen);
+                    if (aciertosFase == faseLen){
+                        faseEntera++;
+                    }
+                }
+            }
+        }
+        puntaje = aciertos*Main.puntosXAcierto + rondaEntera*100 + faseEntera*1000;
     }
 }
 
